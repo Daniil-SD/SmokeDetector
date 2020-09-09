74ccdde91b96b201dd7c38b3938fd3efd4f51e74
Daniil-SD <53513588+Daniil-SD@users.noreply.github.com>
2020-09-09 17:07:56 +0100

    Update findspam.py

--- findspam.py
+++ findspam.py
@@ -44,6 +44,8 @@ CHARACTER_USE_RATIO = 0.42
 PUNCTUATION_RATIO = 0.42
 REPEATED_CHARACTER_RATIO = 0.20
 IMG_TXT_R_THRES = 0.7
+OLD_VIDEO_THRES = 5
+OLD_MEDIUM_POST_THRES = 7
 EXCEPTION_RE = r"^Domain (.*) didn't .*!$"
 RE_COMPILE = regex.compile(EXCEPTION_RE)
 COMMON_MALFORMED_PROTOCOLS = [
@@ -620,6 +622,49 @@ def mostly_img(s, site):
     if s_len_img / len(s) > IMG_TXT_R_THRES:
         return True, "{:.4f} of the post is html image blocks".format(s_len_img / len(s))
     return False, ""
+
+
+def is_recent(date, now, thres):
+    return now.year == int(date[2]) and now.strftime("%b") == date[0] and now.day <= int(date[1]) + thres
+
+
+def scrap_and_check(url_list, date_regex, thres, thing):
+    now = datetime.now()
+    for link in url_list:
+        try:
+            resp = requests.get(link).text
+            date = regex.findall(date_regex, resp)
+            if len(date) == 1 and is_recent(date[0], now, thres):
+                return True, "{} is posted on {} {}, {}".format(thing, date[0][0],
+                                                                date[0][1], date[0][2])
+        except Exception:
+            pass
+    return False, ""
+
+
+@create_rule("Newly posted youtube video")
+def new_video(s, site):
+    # Youtube ID regex is by brunodles, https://stackoverflow.com/a/31711517
+    youtube_ids = regex.findall(r"(?:https?:\/\/)?(?:www\.)?youtu\.?be(?:\.com)?\/" +
+                                r"?.*(?:watch|embed)?(?:.*v=|v\/|\/)([\w\-_]+)\&?", s)
+    youtube_links = ["https://youtu.be/" + x for x in youtube_ids]
+    return scrap_and_check(youtube_links,
+                           r'"dateText":{"simpleText":"(Jan|Feb|Mar|Apr|May|Jun|' +
+                           r'Jul|Aug|Sep|Oct|Nov|Dec)[a-z]? (\d++), (\d++)"}',
+                           OLD_VIDEO_THRES,
+                           "Video")
+
+
+@create_rule("Newly posted medium post")
+def new_medium_post(s, site):
+    medium_links_core = regex.findall(r"medium\.com\/@?[\w-]*+\/[\w-]*+", s)
+    medium_links = ["https://" + x for x in medium_links_core]
+    return scrap_and_check(medium_links,
+                           r'<a class="bh bi at au av aw ax ay az ba fu bd bl bm" rel="noopener" ' +
+                           r'href="[^"]*+">(Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)[a-z]? ' +
+                           r'(\d++), (\d++)<\/a>',
+                           OLD_MEDIUM_POST_THRES,
+                           "Medium post")
 
 
 # noinspection PyUnusedLocal,PyMissingTypeHints
@@ -1868,7 +1913,7 @@ city_list = [
     "Udaipur", "Uttarakhand",
     "Visakhapatnam", "Worli",
     # not in India
-    "Dubai", "Lahore", "Lusail", "Portland",
+    "Dubai", "Lusail", "Portland",
     # yes, these aren't cities but...
     "Abu Dhabi", "Abudhabi", "India", "Malaysia", "Pakistan", "Qatar",
     # buyabans.com spammer uses creative variations
@@ -1992,6 +2037,9 @@ create_rule("bad keyword in {}",
 create_rule("potentially bad keyword in {}", r'^v\w{3,5}\Wkumar$',
             title=False, body=False, username=True,
             all=False, sites=['scifi.stackexchange.com'])
+# Eltima: Nested lookarounds for length limit
+create_rule("bad keyword in {}", r"(?is)\beltima(?<=^(?=.{0,750}$).*)",
+            title=False, max_rep=52)
 create_rule("bad keyword in {}",
             r"(?i)\b(?:(?:beauty|skin|health|face|eye)[- ]?(?:serum|therapy|hydration|tip|renewal|shop|store|lyft|"
             r"product|strateg(?:y|ies)|gel|lotion|cream|treatment|method|school|expert)|fat ?burn(?:er|ing)?|"
